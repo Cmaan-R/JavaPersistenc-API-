078087e937e590e1cb7c75989837dc0d56172c3e
Cmaan-R <robsonleandrox@hotmail.com>
2022-06-08 07:46:48 -0300

    Update

--- exercicios-jpa/src/infra/DAO.java
+++ exercicios-jpa/src/infra/DAO.java
@@ -56,6 +56,10 @@ public class DAO<E> {
 		return this.obterTodos(10 , 0);
 	}
 	
+	public E obterPorID (Object id) {
+		return em.find(classe, id);
+	}
+	
 	
 	
 	public List<E> obterTodos(int qtde, int deslocamento) {
